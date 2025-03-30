5fc4540fed7a270e9e650bd5fccdfff9b32781ff
Boris Dimitrov <borisd2000@gmail.com>
2025-03-30 19:35:44 +0200

    Day 9

---
+++ Day08/ex5_solved.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 900, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Exercise 1")
+
+def square(posX, posY, size, color = (255, 0, 0)):
+    pygame.draw.rect(screen, color, (posX, posY, size, size))
+
+
+
+
+
+
+
+
+
+
+
+def draw():
+    for y in range(40):
+        for x in range(40):
+            square(x * 20, y * 20, 15, (x * 5, y * 5, 200))
+
+
+
+
+
+
+
+
+
+
+draw()
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
---
+++ Day09/ex4.py
@@ -0,0 +1,40 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 900, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Exercise 1")
+
+def square(posX, posY, size, color = (255, 0, 0)):
+    pygame.draw.rect(screen, color, (posX, posY, size, size))
+
+
+
+
+
+
+
+
+
+
+
+def draw():
+    ...
+
+
+
+
+
+
+
+
+
+
+draw()
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
